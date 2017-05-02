2c7d0a7f0a12c99021d419bfb63f5a099d7aa2f2
Jérôme Braeken <jeromebraeken@gmail.com>
2017-05-02 23:25:37 +0200

    added nav links

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ function getJSON(data, element) {
       resolve.data.map((item) =>
         content += 
         `<p class="${item.Country.replace(' ', '_')}">
-          Land: ${item.Country}, Jaar: ${item.Year}
+          Country: ${item.Country}, Total overall: ${item.Total_overall_per_10k}
         </p>`
       )
 
@@ -20,25 +20,86 @@ function getJSON(data, element) {
     })
 }
 
+// NAVIGATION
 function navHandling() {
   const nav = document.querySelector('#nav-all')
   nav.addEventListener('click', (e) => {
     switch(e.target.id) {
-      case 'nav-drug':
-        
-        break;
- 
-      case 'nav-ats':
+      //drug use
+      case 'nav-druguse':
         getJSON('./data/content_druguse.json', document.querySelector('#content'))
         break;
-
-      case 'nav-cocaine':
-        // code
+ 
+      case 'nav-druguse-ats':
+        getJSON('./data/content_druguse_ats.json', document.querySelector('#content'))
         break;
 
-      case 'nav-heroin':
+      case 'nav-druguse-cocaine':
+        getJSON('./data/content_druguse_cocaine.json', document.querySelector('#content'))
         break;
-        // code
+
+      case 'nav-druguse-cannabis':
+        getJSON('./data/content_druguse_cannabis.json', document.querySelector('#content'))
+        break;
+
+      //happiness
+      case 'nav-happiness':
+        getJSON('./data/content_happiness.json', document.querySelector('#content'))
+        break;
+
+      //overdose deaths
+      case 'nav-overdose-deaths':
+        console.log('clicked')
+        getJSON('./data/content_overdose-deaths.json', document.querySelector('#content'))
+        break;
+
+      //price
+      case 'nav-price':
+        getJSON('./data/content_price.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-price-ats':
+        getJSON('./data/content_price_ats.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-price-cocaine':
+        getJSON('./data/content_price_cocaine.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-price-cannabis':
+        getJSON('./data/content_price_cannabis.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-price-heroin':
+        getJSON('./data/content_price_heroin.json', document.querySelector('#content'))
+        break;
+
+
+      //law offences
+      case 'nav-law-offences':
+        getJSON('./data/content_offences_total.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-law-offences-ats':
+        getJSON('./data/content_offences_ats.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-law-offences-cocaine':
+        getJSON('./data/content_offences_cocaine.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-law-offences-cannabis':
+        getJSON('./data/content_offences_cannabis.json', document.querySelector('#content'))
+        break;
+
+      case 'nav-law-offences-heroin':
+        getJSON('./data/content_offences_heroin.json', document.querySelector('#content'))
+        break;
+
+
+
+
+
       default:
         console.error('Nothing clicked')
     }
